091ab71509f8be9b8aa17448617090b09254323c
04samarthg <04samarthgupta@gmail.com>
2025-08-02 15:32:01 +0530

    fixed preflight request errors

--- shakescript/backend/app/main.py
+++ shakescript/backend/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import APIKeyHeader
-
 from app.api.routes import (
     stories_routes,
     episodes_routes,
@@ -20,22 +19,31 @@ app = FastAPI(
     dependencies=[Depends(api_key_header)],
 )
 
-# Configure CORS
+# Configure CORS 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://shakescriptai.tech", "http://localhost:5173"],  
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allow_headers=[
+        "Accept",
+        "Accept-Language",
+        "Content-Language",
+        "Content-Type",
+        "Authorization",
+        "X-Requested-With",
+        "Origin",
+        "Access-Control-Request-Method",
+        "Access-Control-Request-Headers",
+    ],
+    expose_headers=["*"],
 )
 
-
 # Include all the different API routers
-app.include_router(auth_routes.router, prefix="/api/v1" ,tags=["authentication"])
-app.include_router(stories_routes.router, prefix="/api/v1",tags=["stories"])
-app.include_router(episodes_routes.router, prefix="/api/v1",tags=["episodes"])
-app.include_router(dashboard_routes.router, prefix="/api/v1",tags=["dashboard"])
-
+app.include_router(auth_routes.router, prefix="/api/v1", tags=["authentication"])
+app.include_router(stories_routes.router, prefix="/api/v1", tags=["stories"])
+app.include_router(episodes_routes.router, prefix="/api/v1", tags=["episodes"])
+app.include_router(dashboard_routes.router, prefix="/api/v1", tags=["dashboard"])
 
 @app.get("/", tags=["Root"])
 def read_root():
