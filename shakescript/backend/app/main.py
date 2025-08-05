97ae6f6a9f1e4acbf4cc43ed805c661c5c1ee7d6
04samarthg <04samarthgupta@gmail.com>
2025-08-05 11:04:47 +0530

    fixed cors error allow all

--- shakescript/backend/app/main.py
+++ shakescript/backend/app/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://shakescriptai.tech", "http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allow_headers=["*"],
